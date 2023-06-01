ea8b04cb2c90bdf31d59079d47a37772dd9fc453
291kiki <126933139+291kiki@users.noreply.github.com>
2023-06-01 15:00:48 +0900

    .

--- helpmetodripcoffee/hario/darkroasting/script.js
+++ helpmetodripcoffee/hario/darkroasting/script.js
@@ -1,21 +1,7 @@
-setTimeout(function() {
-    var text1 = document.getElementById("text1");
-    text1.style.opacity = "1";
-    setTimeout(function() {
-        text1.style.opacity = "0";
-        setTimeout(function() {
-            var text2 = document.getElementById("text2");
-            text2.style.opacity = "1";
-            setTimeout(function() {
-                text2.style.opacity = "0";
-                setTimeout(function() {
-                    var text3 = document.getElementById("text3");
-                    text3.style.opacity = "1";
-                    setTimeout(function() {
-                        text3.style.opacity = "0";
-                    }, 15000);
-                }, 1000);
-            }, 15000);
-        }, 1000);
-    }, 15000);
-}, 0);
+document.addEventListener("DOMContentLoaded", function() {
+  var shape = document.getElementById("shape");
+
+  setInterval(function() {
+    shape.style.animation = "shrink 25s infinite";
+  }, 25000); // 25 seconds
+});
