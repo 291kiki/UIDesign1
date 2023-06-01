cd526baf24386205cfdc245a863eec9353b559b5
291kiki <126933139+291kiki@users.noreply.github.com>
2023-06-01 17:30:22 +0900

    20230601

--- helpmetodripcoffee/hario/darkroasting/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var shape = document.getElementById("shape");
-
-  setInterval(function() {
-    shape.style.animation = "shrink 25s infinite";
-  }, 25000); // 25 seconds
-});
